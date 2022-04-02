19be4b9a45557818db186028deede5ccc2be09a9
Taylor Balbi <me@taylorbalbi.com>
2022-04-02 11:56:06 -0700

    Fixed formatting and lint issues

--- src/elo/elo.test.ts
+++ src/elo/elo.test.ts
@@ -1,23 +1,23 @@
-import Elo from './index'
+import Elo from './index';
 
 describe('Test Elo Functionality', () => {
-    test('It should calculate expected score', () => {
-        const elo = new Elo()
-        expect(elo.expectedPoints(1000,1000, 400)).toBe(0.5)
-        expect(elo.expectedPoints(800,1000, 400)).toBeCloseTo(0.240)
-        expect(elo.expectedPoints(1200,1000, 400)).toBeCloseTo(0.759)
-    })
-    test('It should calculate new rating', () => {
-        const elo = new Elo()
-        expect(elo.calculate(1000,1000,1)).toBe(1016)
-        expect(elo.calculate(1000,1000,0)).toBe(984)
-        expect(elo.calculate(1000,1000,0.5)).toBe(1000)
+  test('It should calculate expected score', () => {
+    const elo = new Elo();
+    expect(elo.expectedPoints(1000, 1000, 400)).toBe(0.5);
+    expect(elo.expectedPoints(800, 1000, 400)).toBeCloseTo(0.24);
+    expect(elo.expectedPoints(1200, 1000, 400)).toBeCloseTo(0.759);
+  });
+  test('It should calculate new rating', () => {
+    const elo = new Elo();
+    expect(elo.calculate(1000, 1000, 1)).toBe(1016);
+    expect(elo.calculate(1000, 1000, 0)).toBe(984);
+    expect(elo.calculate(1000, 1000, 0.5)).toBe(1000);
 
-        // test K Factor
-        expect(elo.calculate(1000,1000,1, 40)).toBe(1020)
-        expect(elo.calculate(1000,1000,0, 40)).toBe(980)
+    // test K Factor
+    expect(elo.calculate(1000, 1000, 1, 40)).toBe(1020);
+    expect(elo.calculate(1000, 1000, 0, 40)).toBe(980);
 
-        // test 20 K Factor and 200 Performance Rating
-        expect(elo.calculate(1200,1000,0, 20)).toBeCloseTo(1184.805)
-    })
-})
+    // test 20 K Factor and 200 Performance Rating
+    expect(elo.calculate(1200, 1000, 0, 20)).toBeCloseTo(1184.805);
+  });
+});
--- src/elo/index.ts
+++ src/elo/index.ts
@@ -1,46 +1,45 @@
 export default class Elo {
-    private _k : number
-    private _p : number
+  private _k: number;
+  private _p: number;
 
-    /**
-     * Elo rating calculator
-     * @param k {number} The K Factor for calculating ratings (default: 32)
-     * @param p {number} The Performance Rating for calculating ratings (default: 400)
-     */
-    constructor (k : number = 32, p : number = 400) {
-        this._k = k
-        this._p = p
+  /**
+   * Elo rating calculator
+   * @param k {number} The K Factor for calculating ratings (default: 32)
+   * @param p {number} The Performance Rating for calculating ratings (default: 400)
+   */
+  constructor(k: number = 32, p: number = 400) {
+    this._k = k;
+    this._p = p;
+  }
+
+  /**
+   * Calculates the win chance of ratingOne compared to ratingTwo
+   * @param ratingOne {number} The rating of the player for which you are calculating the win chance
+   * @param ratingTwo {number} The rating of the player's opponent
+   * @param p {number} Performance Rating
+   * @returns {number}
+   */
+  public expectedPoints = (ratingOne: number, ratingTwo: number, p: number): number => {
+    return 1 / (1 + Math.pow(10, (ratingTwo - ratingOne) / p));
+  };
+
+  /**
+   * Calculates the delta of the ratings between two players based on K Factor and Performance Rating
+   * @param ratingOne {number} The rating of the player for which you are calculating the new rating
+   * @param ratingTwo {number} The rating of the player's opponent
+   * @param points {1|0|0.5} Win = 1, Loss = 0, Draw = 0.5
+   * @param k {number} (Optional) Overrides K Factor
+   * @returns {number}
+   */
+  public calculate = (ratingOne: number, ratingTwo: number, points: 1 | 0.5 | 0, k?: number, p?: number): number => {
+    if (![1, 0.5, 0].includes(points)) {
+      throw new Error('Invalid points provided to Elo calculation. Please use 0, 0.5 or 1');
     }
 
-    /**
-     * Calculates the win chance of ratingOne compared to ratingTwo
-     * @param ratingOne {number} The rating of the player for which you are calculating the win chance
-     * @param ratingTwo {number} The rating of the player's opponent
-     * @param p {number} Performance Rating
-     * @returns {number}
-     */
-    public expectedPoints = (ratingOne : number, ratingTwo : number, p : number) : number => {
-        
-        return 1 / (1 + Math.pow(10, (ratingTwo - ratingOne) / p))
-    }
+    const factor = !k ? this._k : k;
+    const performance = !p ? this._p : p;
 
-    /**
-     * Calculates the delta of the ratings between two players based on K Factor and Performance Rating
-     * @param ratingOne {number} The rating of the player for which you are calculating the new rating
-     * @param ratingTwo {number} The rating of the player's opponent
-     * @param points {1|0|0.5} Win = 1, Loss = 0, Draw = 0.5
-     * @param k {number} (Optional) Overrides K Factor
-     * @returns {number}
-     */
-    public calculate = (ratingOne : number, ratingTwo : number, points : 1|0.5|0, k? : number, p? : number) : number => {
-        if(![1,0.5,0].includes(points)) {
-            throw new Error("Invalid points provided to Elo calculation. Please use 0, 0.5 or 1")
-        }
-        
-        let factor = (!k) ? this._k : k
-        let performance = (!p) ? this._p : p
-
-        let expected = this.expectedPoints(ratingOne, ratingTwo, performance)
-        return ratingOne + (factor * (points - expected))
-    }
+    const expected = this.expectedPoints(ratingOne, ratingTwo, performance);
+    return ratingOne + factor * (points - expected);
+  };
 }
